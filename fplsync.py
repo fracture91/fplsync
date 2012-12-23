2612a3e23f3d2c6e6416bb68cd45c1e704f32044
Andrew Hurle <fracture91@gmail.com>
2012-12-23 02:00:08 -0500

    Add SongIndex, get Config and CLI argument parsing working

--- fplsync.py
+++ fplsync.py
@@ -2,23 +2,34 @@
 
 import re
 import os
+import argparse
 
 class Config:
-	"""Holds all config info"""
-	self.playlist_dir = None
-	self.source_dir = None
-	self.dest_dir = None
-	self.dest_playlist_dir = None
+	"""Holds all config info - must not be altered after it's passed off to a consumer"""
+	def __init__(self):
+		# see ArgumentParser below - set default values
+		self.playlist_source = None
+		self.source = None
+		self.dest = None
+		self.playlists = []
+		self.playlist_dest = None
+		self.fb2k_source_mapping = None
+		self.dry_run = False
+	def validate(self):
+		# TODO
+		pass
+	def __repr__(self):
+		return "Config {" + ', '.join("%s: %s" % item for item in vars(self).items()) + "}"
 
 class Song:
 	"""Describes a song (which is just some file path)"""
 	def __init__(self, windows_path):
 		"""Create a song with the given windows path
 		
-		windows_path must be whatever was originally in the fpl file
-		ex: F:\Music\Trucker's Atlas.mp3
+		windows_path must be whatever was originally in the fpl file, normalized with abspath
+		ex: os.ntpath.abspath("F:\Music\Trucker's Atlas.mp3")
 		"""
-		self._windows_path = os.ntpath.windows_path
+		self.windows_path = windows_path
 	def get_relative_path(self, source, win_prefix=None):
 		""""""
 	def get_source_path(self, source, falsePrefix=None):
@@ -30,7 +41,7 @@ class Song:
 
 class Playlist:
 	"""Holds a list of songs"""
-	def __init__(self, name, fpl):
+	def __init__(self, name, fpl, song_index):
 		"""Create a playlist
 
 		name - the name of the playlist
@@ -39,12 +50,27 @@ class Playlist:
 		self.name = name
 		self.fpl = fpl
 		self.songs = []
+		self.song_index = song_index
 		with open(self.fpl, 'rb') as infile:
 			# FPL entries have file URIs surrounded by null bytes
 			paths = re.findall(b'(?<=\x00file://)[^\x00]*(?=\x00)', infile.read())
 			# decode the paths as utf-8 and create our songs array
-			self.songs = [Song(path.decode('utf-8')) for path in paths]
+			self.songs = [self.song_index.get_song(path.decode('utf-8')) for path in paths]
 	
+
+class SongIndex:
+	"""Holds map of windows paths to Songs
+
+	Since the same song can be used many times even among a single playlist, we don't want to create
+	a whole bunch of duplicate paths in memory
+	"""
+	def __init__(self):
+		self.songs = {} # windows path -> Song instance
+	def get_song(self, windows_path):
+		normalized = os.ntpath.abspath(windows_path)
+		if not normalized in self.songs:
+			self.songs[normalized] = Song(normalized)
+		return self.songs[normalized]
 
 class PlaylistIndex:
 	"""Responsible for getting named playlists from fb2k"""
@@ -58,15 +84,38 @@ class PlaylistIndex:
 		self.playlists_path = playlists_path
 		self.fpl_files = {} # name -> fpl path
 		self.playlists = {} # name -> playlist
+		self.song_index = SongIndex()
 		# TODO: parse index.dat, fill in fpl_files dict
 	def get_playlist(self, name):
 		"""Get the playlist with the given name, raises KeyError if it does not exist"""
 		if not name in self.playlists:
 			if name in self.fpl_files:
-				self.playlists[name] = Playlist(name, self.fpl_files[name])
+				self.playlists[name] = Playlist(name, self.fpl_files[name], self.song_index)
 			else:
 				raise KeyError("Playlist " + name + " does not exist")
 		return self.playlists[name]
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Sync foobar2000 playlists and their songs")
+	parser.add_argument("playlist-source", help="directory containing foobar2000's FPL files")
+	parser.add_argument("source", help="directory where all songs in FPLs are stored under")
+	parser.add_argument("dest", help="directory to copy songs to - ALL EXTRANEOUS FILES DELETED")
+	parser.add_argument("playlists", nargs='+', help="at least one playlist name, for which the\
+	                    contained songs will be copied into DEST")
+	parser.add_argument("--playlist-dest", help="if specified, copy PLAYLISTS to this directory as\
+	                    m3u8 files that use relative paths to point to their songs in DEST")
+	parser.add_argument("--fb2k-source-mapping", help="absolute path to the SOURCE directory that\
+	                    foobar2000 uses, if different than SOURCE (e.g. 'F:\Music')")
+	parser.add_argument("--dry-run", "-n", action='store_true', help="print what will happen, but\
+	                    don't actually copy or delete any files - highly recommended before a real\
+						run")
+	
+	# create a Config instance and set its properties according to command line args
+	config = parser.parse_args(namespace=Config())
+	config.validate()
+	print(config)
+	# TODO: actually do stuff
+
 
 """
 consumer provides .foobar/playlists, source music mnt, dest music dir,
